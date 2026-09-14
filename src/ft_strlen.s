9de62c37a46ece8c30b2cc9d297a7e2ef6f8a296
Daniel-Escamilla <Daniel.escamilla.bq@gmail.com>
2026-09-14 15:21:27 +0200

    feat: implement ft_strlen function in assembly

---
+++ src/ft_strlen.s
@@ -0,0 +1,22 @@
+global ft_strlen
+
+section .data
+label: db "Frase a analizar", 0
+
+section .text
+ft_strlen:
+	xor rdx, rdx
+	jmp _loop
+	_end:
+	mov rax, rdx
+	ret
+
+_loop:
+	cmp byte [rdi], 0
+	je _end
+	inc rdi
+	add rdx, 1
+	jmp _loop
+
+
+section .note.GNU-stack noalloc noexec nowrite
