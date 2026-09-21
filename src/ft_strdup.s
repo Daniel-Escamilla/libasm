83fb70a74105e7f2bab75a8365ab63545653da47
Daniel-Escamilla <Daniel.escamilla.bq@gmail.com>
2026-09-21 13:40:02 +0200

    fix: update ft_strdup to use correct register for malloc and improve code clarity

--- src/ft_strdup.s
+++ src/ft_strdup.s
@@ -4,19 +4,19 @@ extern ft_strlen
 extern ft_strcpy
 extern malloc
 
-
+section .text
 ft_strdup:
 	push rdi
 	call ft_strlen
 	
 	mov rdi, rax
 	add rdi, 1
-	call malloc
+	call malloc wrt ..plt
 
 	cmp rax, 0
 	jz _error
 	
-	mov rdx, rax
+	mov rdi, rax
 	pop rsi
 	call ft_strcpy
 	ret
