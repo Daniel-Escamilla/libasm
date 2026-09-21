0dd2d7dccfc21b679656289fbb17a3e8bbaabfa6
Daniel-Escamilla <Daniel.escamilla.bq@gmail.com>
2026-09-21 13:01:28 +0200

    feat: add ft_write function in assembly

---
+++ src/ft_write.s
@@ -0,0 +1,20 @@
+global ft_write
+
+extern __errno_location
+
+ft_write:
+	mov eax, 1
+	syscall
+	cmp eax, 0
+	js _error
+	ret
+
+_error:
+	mov ebx, eax
+	neg ebx
+	call __errno_location wrt ..plt
+	mov [rax], ebx
+	mov rax, -1
+	ret
+
+section .note.GNU-stack noalloc noexec nowrite
