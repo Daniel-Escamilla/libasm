ff4218f16f848c85da061bd051e7b9db21f227b1
Daniel-Escamilla <Daniel.escamilla.bq@gmail.com>
2026-09-21 13:02:22 +0200

    feat: implement ft_read function in assembly

---
+++ src/ft_read.s
@@ -0,0 +1,20 @@
+global ft_read
+
+extern __errno_location
+
+ft_read:
+	mov eax, 0
+	syscall
+	cmp eax, 0
+	js _error
+	ret
+
+_error:
+	mov ebx, eax
+	neg ebx
+	call __errno_location wrt ..plt
+	mov [rax], ebx
+	mov rax, -1
+	ret
+
+section .note.GNU-stack noalloc noexec nowrite
